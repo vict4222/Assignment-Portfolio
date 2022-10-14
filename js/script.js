74f668d72c95078781a34ffc094a567d79553880
Victor Ly <vict4222@stud.kea.dk>
2022-10-14 13:26:01 +0200

    burger menu

--- js/script.js
+++ js/script.js
@@ -8,11 +8,17 @@ window.addEventListener("DOMContentLoaded", init);
 
 // initialize
 function init() {
-  HTML.switch = document.querySelector("#theme-toggle");
+  HTML.switch = document.querySelector("#theme-toggle"); // theme toggle button
   HTML.switch.addEventListener("click", toggleThemes);
-
   document.documentElement.setAttribute("data-theme", theme === "light" ? "light" : "dark");
+
+  HTML.burgerMenu = document.querySelector("#burger-menu"); // burger menu button
+  HTML.navMenu = document.querySelector("#nav-items"); // nav menu
+  HTML.nav = document.querySelector("nav"); // nav bar
+  HTML.burgerMenu.addEventListener("click", toggleMenu);
 }
+
+// dark mode / light mode
 
 // toggle between light and dark theme
 function toggleThemes() {
@@ -40,4 +46,17 @@ function changeThemeToDark() {
   document.documentElement.setAttribute("data-theme", "dark"); // set theme to dark
   localStorage.setItem("data-theme", "dark"); // save theme to local storage
   HTML.switch.dataset.toggle = "dark";
+}
+
+// burger menu
+
+// open and close burger menu
+function toggleMenu() {
+  if (HTML.nav.classList.contains("active")) {
+    HTML.nav.classList.remove("active");
+    HTML.burgerMenu.textContent = "☰";
+  } else {
+    HTML.nav.classList.add("active");
+    HTML.burgerMenu.textContent = "×";
+  }
 }
