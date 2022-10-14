9fa26aa1352a5697c29220ebcdbabdac76c76f61
Victor Ly <vict4222@stud.kea.dk>
2022-10-14 12:22:39 +0200

    dark mode

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 "use strict";
+
+const HTML = []; // global variables
+let theme = localStorage.getItem("data-theme") ?? localStorage.setItem("data-theme", "light"); // default is light
+
+// wait for the document to finish loading
+window.addEventListener("DOMContentLoaded", init);
+
+// initialize
+function init() {
+  HTML.switch = document.querySelector("#theme-toggle");
+  HTML.switch.addEventListener("click", toggleThemes);
+
+  document.documentElement.setAttribute("data-theme", theme === "light" ? "light" : "dark");
+}
+
+// toggle between light and dark theme
+function toggleThemes() {
+  let theme = localStorage.getItem("data-theme");
+  switch (theme) {
+    case "dark":
+      changeThemeToLight();
+      break;
+
+    case "light":
+      changeThemeToDark();
+      break;
+  }
+}
+
+// change to light theme
+function changeThemeToLight() {
+  document.documentElement.setAttribute("data-theme", "light"); // set theme to light
+  localStorage.setItem("data-theme", "light"); // save theme to local storage
+  HTML.switch.dataset.toggle = "light";
+}
+
+// change to dark theme
+function changeThemeToDark() {
+  document.documentElement.setAttribute("data-theme", "dark"); // set theme to dark
+  localStorage.setItem("data-theme", "dark"); // save theme to local storage
+  HTML.switch.dataset.toggle = "dark";
+}
