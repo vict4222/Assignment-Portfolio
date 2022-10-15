7449010e364049c6bf0b96609515d2af86ab7f5d
Victor Ly <vict4222@stud.kea.dk>
2022-10-15 16:09:21 +0200

    simple motion one animation

--- js/motion-one.js
+++ js/motion-one.js
@@ -1,3 +1,13 @@
 "use strict";
 
-import { inView, animate } from "https://cdn.skypack.dev/motion";
+import { stagger, timeline } from "https://cdn.skypack.dev/motion";
+
+const sequence = [
+  //
+  [".animate", { opacity: [0, 1], y: [10, 0] }, { duration: 0.6, delay: stagger(0.15) }],
+  ["#about-container", { opacity: [0, 1], y: [10, 0] }, { duration: 0.6 }],
+];
+
+const options = { delay: 0.5 };
+
+timeline(sequence, options);
